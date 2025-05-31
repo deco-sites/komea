b5971086178ac028c17a8a90e30555eba86904c2
EduardoBalbinot <eduardo.balbinot.dev@gmail.com>
2025-05-31 02:52:16 -0300

    feat: RevealingText lines now also fade-in instead of the original animation

--- sections/RevealingText.tsx
+++ sections/RevealingText.tsx
@@ -1,16 +1,14 @@
 import { RichText, ImageWidget } from "apps/admin/widgets.ts";
 import { useId } from "../sdk/useId.ts";
 import { useScript } from "@deco/deco/hooks";
-import Image from "apps/website/components/Image.tsx";
 
-const onLoad = (rootId: string, lines: Line[], speed: number) => {
+const onLoad = (rootId: string, lines: Line[]) => {
   const parent = document.getElementById(rootId) as HTMLElement;
   const sticky = parent.querySelector('.sticky') as HTMLElement;
   const lineElements: NodeListOf<HTMLElement> = parent.querySelectorAll(".line");
 
-  const windowHeight = globalThis.innerHeight;
-
   let progressPercent = 0;
+  const eachLinePercentage = 100 / lines.length;
 
   const handleScroll = () => {
     const parentRect = parent.getBoundingClientRect();
@@ -22,7 +20,12 @@ const onLoad = (rootId: string, lines: Line[], speed: number) => {
     progressPercent = (distanceFromParentTop / parentHeight) * 100;
 
     lineElements.forEach((line, index) => {
-      line.style.background = `linear-gradient(to right, ${lines[index].color} ${(distanceFromParentTop * speed) - 10 - (index * windowHeight * 0.1)}%, transparent ${(distanceFromParentTop * speed) - (index * windowHeight * 0.1)}%)`
+      const disconsideredPercentage = eachLinePercentage * index;
+      const currentPorcentage = (progressPercent - disconsideredPercentage) * (index + lines.length);
+      if (lines[index].justReveal) line.style.opacity = (currentPorcentage / 100).toString();
+      else line.style.background = `linear-gradient(to right, ${lines[index].color} ${(currentPorcentage - 10)}%, transparent ${(currentPorcentage)}%)`;
+      //if (lines[index].justReveal) line.style.opacity = (distanceFromParentTop - (windowHeight * index)).toString();
+      //else line.style.background = `linear-gradient(to right, ${lines[index].color} ${(distanceFromParentTop * speed) - 10 - (index * windowHeight * 0.1)}%, transparent ${(distanceFromParentTop * speed) - (index * windowHeight * 0.1)}%)`
     })
   };
 
@@ -53,30 +56,29 @@ export interface Line {
   /** @format color-input */
   color?: string;
   textProps?: TextProps;
+  justReveal?: boolean;
 }
 
 export interface Props {
   lines?: Line[];
   backgroundMedia?: BackgroundMedia
-  /** @description deafult is 0.1 */
-  speed?: number;
-  /** @description deafult is 185 */
+  /** @description deafult is 120 */
   scrollAmount?: number;
 }
 
-export default function RevealingText({ lines = [], backgroundMedia, scrollAmount, speed }: Props) {
+export default function RevealingText({ lines = [], backgroundMedia, scrollAmount }: Props) {
   const rootId = useId();
-  return <div id={rootId} class="relative min-h-[240vh]" style={{ height: `${lines.length * ((scrollAmount || 185) - lines.length * 10)}vh` }} >
+  return <div id={rootId} class="relative min-h-[120vh]" style={{ height: `${lines.length * (scrollAmount || 120)}vh` }} >
     <script
       type="module"
-      dangerouslySetInnerHTML={{ __html: useScript(onLoad, rootId, lines, speed || 0.1) }}
+      dangerouslySetInnerHTML={{ __html: useScript(onLoad, rootId, lines) }}
     />
-    <div class="h-[100vh] bg-blue-400 sticky top-0 flex flex-col items-center justify-center" style={{ background: backgroundMedia?.backgroundColor }}>
+    <div class="h-[100vh] sticky top-0 flex flex-col items-center justify-center" style={{ background: backgroundMedia?.backgroundColor }}>
       {lines.map(line => (
         <div
           dangerouslySetInnerHTML={{ __html: line.text || "" }}
-          class="!text-transparent !bg-clip-text line leading-none"
-          style={{ ...line.textProps, background: `linear-gradient(to right, ${line.color} 100%, transparent 110%)` }} />
+          class="!text-transparent !bg-clip-text line leading-none transition-opacity duration-300"
+          style={{ ...line.textProps, background: `linear-gradient(to right, ${line.color} 100%, transparent 110%)`, opacity: line.justReveal ? 0 : "" }} />
       ))}
     </div>
   </div>
